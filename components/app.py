983c5d070c1acf00ba0354b70f297e05ac7befff
Nilay Gaitonde <68230775+NilayGaitonde@users.noreply.github.com>
2024-03-21 16:27:09 +0530

    face detection done

--- components/app.py
+++ components/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import torch
+from mtcnn.mtcnn import MTCNN
 import time
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
@@ -23,15 +24,16 @@ def loadmodels():
 
     realvfake = load_model("artifacts/realvfake.h5")
 
-
-    return discrimator, generator, yolo, realvfake
+    mtcnn = MTCNN()
+    return discrimator, generator, yolo, realvfake,mtcnn
 
 def check_human(yolo,img,mtcnn):
-    mtcnn_acc, yolo_acc = compare_models(image=img,pixels=img,yolo=yolo,mtcnn=mtcnn)
-    if mtcnn_acc!=0 and yolo_acc!=0:
+    mtcnn_acc, yolo_acc,model_name = compare_models(pixels=img)
+    if mtcnn_acc>0.0 and yolo_acc>0.0:
+        st.write(f"Using {model_name} for face detection")
         return True
-def plot_generated_images(realfake, yolo,generator,img, square = 5, epochs = 0, latent_dim = 100):
-    if check_human(yolo,img=img):
+def plot_generated_images(mtcnn, realfake, yolo,generator,img, square = 5, epochs = 0, latent_dim = 100):
+    if check_human(yolo=yolo,mtcnn=mtcnn,img=img):
         fig = plt.figure(figsize = (10,10))
         for i in range(square * square):
             if epochs != 0:    
@@ -49,7 +51,7 @@ def plot_generated_images(realfake, yolo,generator,img, square = 5, epochs = 0, 
         return st.write("No person detected in the image")
     
 def main():
-    discrimator, generator, yolo, realvfake = loadmodels()
+    discrimator, generator, yolo, realvfake,mtcnn = loadmodels()
     st.title("Image Uploader")
     st.write("Please upload an image file.")
     option = st.selectbox("Select an option:", ("Take a photo", "Try a Demo"))
@@ -58,14 +60,14 @@ def main():
         image = st.camera_input("Capture image")
         if image is not None:
             image = process_file(image)
-            plot_generated_images(realfake=realvfake,yolo=yolo,generator=generator,img = image, square=2)
+            plot_generated_images(mtcnn=mtcnn,realfake=realvfake,yolo=yolo,generator=generator,img = image, square=2)
 
     elif option == 'Try a Demo':
         image = np.array(Image.open("artifacts/demo.webp"))
         st.image(image)
 
         st.write("#### Generated images")
-        plot_generated_images(realfake=realvfake,yolo=yolo,generator=generator,img = image, square=2)
+        plot_generated_images(mtcnn=mtcnn,realfake=realvfake,yolo=yolo,generator=generator,img = image, square=2)
     
 if __name__ == "__main__":
     main()
--- components/objectdetection.py
+++ components/objectdetection.py
@@ -21,26 +21,28 @@ def draw_image_with_boxes(filename, results, model_name):
 
     image.show(title=f"Model: {model_name}")
 
-def compare_models(filename,pixels):
+def compare_models(pixels,filename=""):
     detector = MTCNN()
     faces = detector.detect_faces(pixels)
     mtcnn_accuracy = sum([face['confidence'] for face in faces]) / len(faces) if faces else 0
 
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
-    results = model(filename)
+    results = model(pixels)
     person_results = [obj for obj in results.xyxy[0] if model.names[int(obj[5])] == 'person']
     yolov5_accuracy = sum([obj[4].item() * 100 for obj in person_results]) / len(person_results) if person_results else 0
 
     mtcnn_accuracy =  mtcnn_accuracy * 100
 
     if mtcnn_accuracy > yolov5_accuracy:
+        model_name='MTCNN'
         print(f"MTCNN has higher accuracy ({mtcnn_accuracy:.2f}%) for face detection compared to YOLOv5 which has ({yolov5_accuracy:.2f}%)")
-        draw_image_with_boxes(filename, faces, 'MTCNN')
+        # draw_image_with_boxes(filename, faces, 'MTCNN')
     else:
+        model_name='YOLOv5'
         print(f"YOLOv5 has higher accuracy ({yolov5_accuracy:.2f}%) for person detection compared to MTCNN which has ({mtcnn_accuracy:.2f}%)")
         results.show()
 
-    return mtcnn_accuracy, yolov5_accuracy
+    return mtcnn_accuracy, yolov5_accuracy,model_name
 
 if __name__=='__main__':
     filename="Photo on 21-03-24 at 15.48.jpg"
