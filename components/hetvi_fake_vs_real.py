8c5c27c6136f88ac24962744763eecad5cbd62b1
Rounak <rounakbachwani@gmail.com>
2024-03-21 21:57:28 +0530

    files updated

--- components/hetvi_fake_vs_real.py
+++ components/hetvi_fake_vs_real.py
@@ -7,23 +7,16 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
 #function for preprocessing the input image
 def preprocess_image(image_path):
-
     img = load_img(image_path, target_size=(224, 224), color_mode='grayscale')
-
     img_array = img_to_array(img)
-
     img_array /= 255.0
-
     img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 #function for making predictions
 def make_prediction(model, image_path):
-
     preprocessed_img = preprocess_image(image_path)
-
     prediction = model.predict(preprocessed_img)
-
     if prediction > 0.5:
         return "Real"
     else:
@@ -32,11 +25,9 @@ def make_prediction(model, image_path):
 def main():
     # Load the pre-trained model
     model = load_model('C:/Users/Rounak/Desktop/OneDrive/College/Projects/Image Anonymization/artifacts/Real VS Fake.h5')  # Change the filename if necessary
-
     # Take user input for the image path
     #image_path = input("Enter the path to the image: ")
-    image_path = 'C:/Users/Rounak/Desktop/OneDrive/College/Projects/Image Anonymization/artifacts/demo.jpeg'
-
+    image_path = 'C:/Users/Rounak/Desktop/OneDrive/College/Projects/Image Anonymization/artifacts/demo.webp'
 
     # Check if the image path is valid
     if not os.path.exists(image_path):
